30508303a706f788b08e66b917f9ca7399991bf6
Josh Mu <mu@joshmu.com>
2020-05-11 14:54:29 +1000

    circles with stagger and click

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import React, { useRef, useEffect } from 'react'
-import logo from './logo.svg'
+import React, { useRef, useEffect, useState } from 'react'
 import './App.css'
 
 import { TweenMax, Power3 } from 'gsap'
 
 function App() {
-  const logoItem = useRef(null)
+  const app = useRef(null)
+  const circle = useRef(null)
+  const circleRed = useRef(null)
+  const circleBlue = useRef(null)
 
-  useEffect(() => {
-    console.log(logoItem)
-    TweenMax.to(logoItem.current, 0.8, {
-      opacity: 1,
-      y: -20,
+  const [expanded, setExpanded] = useState(false)
+
+  const handleExpand = () => {
+    TweenMax.to(circleRed.current, 0.8, {
+      width: expanded ? 75 : 200,
+      height: expanded ? 75 : 200,
       ease: Power3.easeOut,
     })
+    setExpanded(!expanded)
+  }
+
+  useEffect(() => {
+    // console.log(circle, circleRed, circleBlue)
+    TweenMax.to(app.current, 0, { css: { visibility: 'visible' } })
+    // TweenMax.from(circle.current, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    // })
+    // TweenMax.from(circleRed.current, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.2,
+    // })
+    // TweenMax.from(circleBlue.current, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.4,
+    // })
+    TweenMax.staggerFrom(
+      [circle.current, circleRed.current, circleBlue.current],
+      0.8,
+      { opacity: 1, x: 40, ease: Power3.easeOut },
+      0.2
+    )
   }, [])
 
   return (
-    <div className='App'>
+    <div ref={app} className='App'>
       <header className='App-header'>
-        <img ref={logoItem} src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <div className='circle-container'>
+          <div ref={circle} className='circle'></div>
+          <div
+            ref={circleRed}
+            onClick={handleExpand}
+            className='circle red'
+          ></div>
+          <div ref={circleBlue} className='circle blue'></div>
+        </div>
       </header>
     </div>
   )
